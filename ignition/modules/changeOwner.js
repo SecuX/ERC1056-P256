7d10ebe61dd7f7ee04341ca754fb5b94d8f0f849
rus7hex <dick525724@gmail.com>
2024-07-29 09:14:39 +0800

    enhance ignition test

--- ignition/modules/changeOwner.js
+++ ignition/modules/changeOwner.js
@@ -4,7 +4,11 @@ const { p256Wallet } = require("./helpers.js");
 module.exports = {
     default: buildModule(`sx${Date.now()}`, (m) => {
         const erc1056 = m.contractAt("ERC1056P256", process.env.CONTRACT_ADDRESS);
-        m.call(erc1056, "changeOwner", ["0xE2E7A1A1f644c364AB166d3c660c8C3343F78b01", p256Wallet.address]);
+
+        hre.ethers.getSigners()
+            .then(([issuer]) => {
+                m.call(erc1056, "changeOwner", [issuer.address, p256Wallet.address]);
+            });
 
         return { erc1056 };
     })
